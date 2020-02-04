f190179f8105395d854fe4d437250404168a2f33
MGrayce <amgraymofficial@gmail.com>
2020-02-04 18:01:03 +0300

    Added a login activity Changed the code in the main

--- app/src/main/java/com/kunywana/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/kunywana/myfirstapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "This is my first application in android!!! Hello!!!!!!!!!!", Toast.LENGTH_SHORT).show();
+
     }
 }
