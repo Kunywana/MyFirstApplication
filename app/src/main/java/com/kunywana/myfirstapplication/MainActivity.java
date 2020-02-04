1ea4446547449c7b4f87d52e4d0801c0e51dd044
MGrayce <amgraymofficial@gmail.com>
2020-02-04 17:51:14 +0300

    The first commit

---
+++ app/src/main/java/com/kunywana/myfirstapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kunywana.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "This is my first application in android!!! Hello!!!!!!!!!!", Toast.LENGTH_SHORT).show();
+    }
+}
